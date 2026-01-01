f5c0ab72672b1393f7ec3d7ef3452b7828614ceb
BanYeah <04smailing@gmail.com>
2026-01-02 01:21:56 +0900

    Style(fe): Prettier Formatting

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,27 +1,28 @@
 import type { Metadata } from "next";
-import BMJUA from 'next/font/local'
+import BMJUA from "next/font/local";
+import "@mantine/core/styles.layer.css";
 import "./globals.css";
 
 const font = BMJUA({
-  src: '../public/fonts/BMJUA.woff',
+  src: "../public/fonts/BMJUA.woff",
   variable: "--font-BMJUA",
-})
+});
 
 export const metadata: Metadata = {
   title: "패션 피버",
   description: "포켓미니: 패션을 뽐내는 미니들의 축제",
   icons: {
-    "shortcut": "/favicon_io/favicon.ico",
-    "icon": "/favicon_io/android-chrome-512x512.png",
-    "apple": "/favicon_io/apple-touch-icon.png",
+    shortcut: "/favicon_io/favicon.ico",
+    icon: "/favicon_io/android-chrome-512x512.png",
+    apple: "/favicon_io/apple-touch-icon.png",
   },
-  manifest: "/favicon_io/manifest.json"
+  manifest: "/favicon_io/manifest.json",
 };
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" className={font.className}>
@@ -29,5 +30,5 @@ export default function RootLayout({
         <main>{children}</main>
       </body>
     </html>
-  )
+  );
 }
