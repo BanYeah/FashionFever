a620332fe2d1467fdf40eeb715b03c631d08d4cd
BanYeah <04smailing@gmail.com>
2026-01-02 01:23:50 +0900

    Feat(fe): Not Found

---
+++ frontend/app/not-found.tsx
@@ -0,0 +1,18 @@
+export default function NotFound() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        columnGap: "16px",
+        height: "100%",
+        fontSize: "16px",
+      }}
+    >
+      <p>404</p>
+      <p>|</p>
+      <p>페이지를 찾을 수 없습니다.</p>
+    </div>
+  );
+}
