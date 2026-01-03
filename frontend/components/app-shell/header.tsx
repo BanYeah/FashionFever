03bc8c53b7dcb30725c203566c426253d195ac69
yerlyn6v6 <yerlyn6v6@g.skku.edu>
2026-01-04 03:46:11 +0900

    Fix(fe): Header

--- frontend/components/app-shell/header.tsx
+++ frontend/components/app-shell/header.tsx
@@ -1,7 +1,7 @@
-import classes from './header.module.css';
-import Link from 'next/link';
-import Image from 'next/image';
-import { UnstyledButton } from '@mantine/core';
+import classes from "./header.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import { UnstyledButton } from "@mantine/core";
 
 interface AppShellHeaderProps {
   href: string;
@@ -10,32 +10,37 @@ interface AppShellHeaderProps {
   subTitle?: string;
 }
 
-export default function GlobalHeader({ href, gift, title, subTitle }: AppShellHeaderProps) {
+export default function GlobalHeader({
+  href,
+  gift,
+  title,
+  subTitle,
+}: AppShellHeaderProps) {
   return (
-    <div className={classes.container}>
-      <header className={classes.mainHeader}>
+    <div className={classes.Container}>
+      <header className={classes.MainHeader}>
         {/* 뒤로가기 버튼 영역 */}
-        <Link href={href} className={classes.backButton}>
-          <Image 
-            src="/images/app-shell/goback.svg" 
-            alt="뒤로가기" 
-            width={17} 
-            height={25} 
+        <Link href={href} className={classes.BackButton}>
+          <Image
+            src="/images/app-shell/goback.svg"
+            alt="뒤로가기"
+            width={17}
+            height={25}
             priority
           />
         </Link>
-        
-        <div className={classes.title}>{title}</div>
+
+        <div className={classes.Title}>{title}</div>
 
         {/* 우측 선물 버튼 영역 */}
-        <div className={classes.rightSection}>
+        <div className={classes.RightSection}>
           {gift && (
             <UnstyledButton w={30} h={30}>
-              <Image 
-                src="/images/app-shell/present.svg" 
-                alt="선물목록" 
-                width={30} 
-                height={30} 
+              <Image
+                src="/images/app-shell/present.svg"
+                alt="선물목록"
+                width={30}
+                height={30}
               />
             </UnstyledButton>
           )}
@@ -43,13 +48,10 @@ export default function GlobalHeader({ href, gift, title, subTitle }: AppShellHe
       </header>
 
       {subTitle && (
-        <div className={classes.subHeader}>
-          <p className={classes.subText}>{subTitle}</p>
+        <div className={classes.SubHeader}>
+          <p className={classes.SubText}>{subTitle}</p>
         </div>
       )}
     </div>
   );
-
 }
-
-
