0e5b0dc9824052b6d061c3c5f18ba72a79df1423
yerlyn6v6 <yerlyn6v6@g.skku.edu>
2026-01-05 20:16:36 +0900

    Fix(fe): GiftReceive

---
+++ frontend/app/ranking/[id]/top1/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { AppShell } from "@/components/app-shell/app-shell";
+import { AppShellFooter } from "@/components/app-shell/footer";
+import { RankingDisplay } from "@/components/ranking/ranking-display";
+import { GiftReceive } from "@/components/ranking/gift-receive";
+import { Stack } from "@mantine/core"
+export default function RankingPage() {
+  return (
+    <AppShell
+      gift
+      footer={
+        <AppShellFooter
+          variant="tabs"
+          tabs={["나의 최고 랭킹", "나의 랭킹", "상위 랭킹!"]}
+          activeTab={0}
+          tabLinks={["/", "/", "/"]}
+        />
+      }
+    >
+      <Stack gap={29} m={15}><RankingDisplay/>
+      <GiftReceive /></Stack>
+      
+    </AppShell>
+  );
+}
