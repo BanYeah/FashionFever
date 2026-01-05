a1162f1e29eece6cff346209484fce85855b9abd
BanYeah <04smailing@gmail.com>
2026-01-05 22:51:50 +0900

    Feat(fe): AccountSelect

---
+++ frontend/components/theme-setting/account-select.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import "@mantine/core/styles/Popover.css";
+import classes from "./account-select.module.css";
+
+import { Dispatch, SetStateAction } from "react";
+import { Stack, Box, MultiSelect, Select } from "@mantine/core";
+
+interface AccountSelectProps {
+  mt?: number;
+  label: string;
+  value: string | null;
+  setValue: Dispatch<SetStateAction<string | null>>;
+}
+
+export function AccountSelect({
+  mt = 0,
+  label,
+  value,
+  setValue,
+}: AccountSelectProps) {
+  const data = ["React", "Angular", "Vue", "Svelte"];
+
+  return (
+    <Stack mt={mt} gap={9}>
+      <Box px={10}>
+        <p>{label}</p>
+      </Box>
+      <Select
+        classNames={{
+          input: classes.SelectInput,
+          dropdown: classes.SelectDropdown,
+          option: classes.SelectOption,
+        }}
+        data={data}
+        value={value}
+        onChange={setValue}
+        rightSection={null}
+        placeholder="admin"
+        searchable
+        nothingFoundMessage="검색 결과가 없어요"
+        limit={3} // 검색 결과 수 제한
+      />
+    </Stack>
+  );
+}
+
+interface AccountMultiSelectProps {
+  mt?: number;
+  label: string;
+  value: string[];
+  setValue: Dispatch<SetStateAction<string[]>>;
+}
+
+export function AccountMultiSelect({
+  mt = 0,
+  label,
+  value,
+  setValue,
+}: AccountMultiSelectProps) {
+  const data = [
+    "React",
+    "Angular",
+    "Vue",
+    "Svelte",
+    "Next",
+    "Nest",
+    "Express",
+    "FastAPI",
+  ];
+
+  return (
+    <Stack mt={mt} gap={9}>
+      <Box px={10}>
+        <p>{label}</p>
+      </Box>
+      <MultiSelect
+        classNames={{
+          input: classes.MultiSelectInput,
+          dropdown: classes.SelectDropdown,
+          pill: classes.MultiSelectPill,
+        }}
+        data={data}
+        value={value}
+        onChange={setValue}
+        rightSection={null}
+        searchable
+        nothingFoundMessage="검색 결과가 없어요"
+        limit={3} // 검색 결과 수 제한
+      />
+    </Stack>
+  );
+}
