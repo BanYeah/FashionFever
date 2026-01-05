962623ac460d88911369fd0e7bb7516a8485dbb9
yerlyn6v6 <yerlyn6v6@g.skku.edu>
2026-01-06 03:50:42 +0900

    Fix(fe): Voting

--- frontend/components/voting/voting.tsx
+++ frontend/components/voting/voting.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { Box, Stack, UnstyledButton, Text } from "@mantine/core";
-import Image from "next/image";
 import classes from "./voting.module.css";
+import Image from "next/image";
+import { Box, Stack, UnstyledButton } from "@mantine/core";
 
 export function VotingDisplay() {
   const modelImg = "/images/model.png";
@@ -10,40 +10,53 @@ export function VotingDisplay() {
   const sameIcon = "/images/voting/same.svg";
 
   return (
-      <Stack gap={12} px={15} py={12} style={{ position: "relative" }}>
-        {/* 첫 번째 이미지 영역 */}
-        <div style={{ position: "relative", width: "100%", display: "block" }}>
-          <Box className={classes.ImageWrapper}>
-            <Image src={modelImg} alt="Model 1" fill style={{ objectFit: 'cover' }} />
-          </Box>
-          
-          <UnstyledButton className={classes.VoteButton} onClick={() => console.log("Top Like")}>
-            <Image src={likeIcon} alt="Like Top" width={66.7} height={60} />
-          </UnstyledButton>
-        </div>
+    <Stack gap={12} px={15} py={12} style={{ position: "relative" }}>
+      {/* 첫 번째 이미지 영역 */}
+      <div style={{ position: "relative", width: "100%" }}>
+        <Image
+          src={modelImg}
+          alt="Model 1"
+          width={390}
+          height={312}
+          style={{ width: "100%", height: "auto", display: "block" }}
+        />
+        {/* 첫 번째 좋아요 버튼 */}
+        <UnstyledButton
+          className={classes.VoteButton}
+          onClick={() => console.log("Top Like")}
+        >
+          <Image src={likeIcon} alt="Like Top" width={66.7} height={60} />
+        </UnstyledButton>
+      </div>
 
-        {/* 두 버튼 사이 정중앙 무승부 버튼 */}
-        <UnstyledButton className={classes.SameButton} onClick={() => console.log("Same")}>
+      {/* 두 버튼 사이 정중앙 무승부 버튼 */}
+      <UnstyledButton
+        className={classes.SameButton}
+        onClick={() => console.log("Same")}
+      >
         <Image src={sameIcon} alt="Same" width={50} height={50} />
       </UnstyledButton>
-      
-        
 
-        {/* 두 번째 이미지 영역 */}
-        <div style={{ position: "relative", width: "100%", display: "block" }}>
-          <Box className={classes.ImageWrapper}>
-            <Image src={modelImg} alt="Model 2" fill style={{ objectFit: 'cover' }} />
-          </Box>
-          
-          <UnstyledButton className={classes.VoteButton} onClick={() => console.log("Bottom Like")}>
-            <Image src={likeIcon} alt="Like Bottom" width={66.7} height={60} />
-          </UnstyledButton>
-        </div>
-        {/* 검은 선 */}
+      {/* 두 번째 이미지 영역 */}
+      <div style={{ position: "relative", width: "100%", display: "block" }}>
+        <Image
+          src={modelImg}
+          alt="Model 2"
+          width={390}
+          height={312}
+          style={{ width: "100%", height: "auto", display: "block" }}
+        />
+        {/* 두 번째 좋아요 버튼 */}
+        <UnstyledButton
+          className={classes.VoteButton}
+          onClick={() => console.log("Bottom Like")}
+        >
+          <Image src={likeIcon} alt="Like Bottom" width={66.7} height={60} />
+        </UnstyledButton>
+      </div>
+
+      {/* 검은 선 */}
       <Box className={classes.VerticalLine} />
-      </Stack>
-
-      
-      
+    </Stack>
   );
 }
