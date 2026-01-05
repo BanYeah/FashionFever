3b073f260455583cd2c658f2fecd064f4697f886
yerlyn6v6 <yerlyn6v6@g.skku.edu>
2026-01-06 00:06:40 +0900

    HOTFIX

--- frontend/app/ranking/[id]/page.tsx
+++ frontend/app/ranking/[id]/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { AppShell } from "@/components/app-shell/app-shell";
 import { AppShellFooter } from "@/components/app-shell/footer";
 import { RankingCollection } from "@/components/ranking/ranking-collection";
