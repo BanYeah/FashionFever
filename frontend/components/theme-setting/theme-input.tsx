e8f467e62f318f8f6b639e092dcf7e42ce2059da
BanYeah <04smailing@gmail.com>
2026-01-05 19:53:33 +0900

    Feat(fe): ThemeInput

---
+++ frontend/components/theme-setting/theme-input.tsx
@@ -0,0 +1,34 @@
+import classes from "./theme-input.module.css";
+import { Dispatch, SetStateAction } from "react";
+import { Stack, Box, Input } from "@mantine/core";
+
+interface ThemeInputProps {
+  mt?: number;
+  label: string;
+  placeholder: string;
+  value: string;
+  setValue: Dispatch<SetStateAction<string>>;
+}
+
+export function ThemeInput({
+  mt = 0,
+  label,
+  placeholder,
+  value,
+  setValue,
+}: ThemeInputProps) {
+  return (
+    <Stack mt={mt} gap={9}>
+      <Box px={10}>
+        <p>{label}</p>
+      </Box>
+      <Input
+        classNames={{ input: classes.Input }}
+        variant="unstyled"
+        placeholder={placeholder}
+        value={value}
+        onChange={(event) => setValue(event.currentTarget.value)}
+      />
+    </Stack>
+  );
+}
