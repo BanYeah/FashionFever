6b855ebcd4c86799bb9216dc4806797b2cde6a0a
yerlyn6v6 <yerlyn6v6@g.skku.edu>
2026-01-07 03:08:27 +0900

    HOTFIX

--- frontend/components/app-shell/app-shell.tsx
+++ frontend/components/app-shell/app-shell.tsx
@@ -1,26 +1,23 @@
-import { AppShellHeader } from "./header";
-
-type ShellVariant = "theme" | "user-setting" | "judge-setting";
+import { AppShellHeader, AppShellVariant } from "./header";
 
 interface AppShellProps {
+  variant: AppShellVariant;
   gift?: boolean;
   subHeader?: boolean;
   footer: React.ReactNode;
   children: React.ReactNode;
-  variant?: ShellVariant;
 }
 
 export function AppShell({
+  variant,
   gift,
   subHeader,
   footer,
   children,
-  variant,
 }: AppShellProps) {
-  
   return (
     <section style={{ minHeight: "100dvh" }}>
-      <AppShellHeader gift={gift} subHeader={subHeader} variant={variant} />
+      <AppShellHeader variant={variant} gift={gift} subHeader={subHeader} />
       <section
         style={{
           paddingBottom: `${footer !== null ? "60px" : "0px"}`,
