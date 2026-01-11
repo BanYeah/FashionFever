4694a188d667b0adf0b80b7b05dfba2824218401
BanYeah <04smailing@gmail.com>
2026-01-11 17:25:33 +0900

    Feat(be): Setup Swagger UI

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Swagger Configuration
+  const config = new DocumentBuilder()
+    .setTitle('Pocket Mini: Fashion Fever Event API Service')
+    .setDescription(
+      'Web-based Re-implementation of the Pocket Mini: Fashion Fever Event를 위한 API 명세서입니다.',
+    )
+    .setVersion('1.0')
+    .addCookieAuth('connect.sid')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // http://localhost:3000/api로 접속
+
+  await app.listen(3000);
 }
 bootstrap();
