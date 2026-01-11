b2793c3c2c2163c283fe96465246ade0e312adb7
BanYeah <04smailing@gmail.com>
2026-01-11 16:58:18 +0900

    Feat(be): Setup PostgreSQL Connection and ConfigService

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('POSTGRES_HOST'),
+        port: configService.get<number>('POSTGRES_PORT'),
+        username: configService.get<string>('POSTGRES_USER'),
+        password: configService.get<string>('POSTGRES_PASSWORD'),
+        database: configService.get<string>('POSTGRES_DATABASE_NAME'),
+
+        // 각 모듈에서 TypeOrmModule.forFeature([User])와 같이 등록된 엔티티들 자동 수집
+        autoLoadEntities: true,
+
+        // 개발 환경에서 엔티티 변경 시 DB 테이블 자동 수정 (배포 환경에서는 false)
+        synchronize: configService.get<string>('NODE_ENV') === 'development',
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
