f4002a209c6052d7040b86932e7c4c34f965aac2
BanYeah <04smailing@gmail.com>
2026-01-27 23:16:44 +0900

    Feat(fe): Middleware

---
+++ frontend/middleware.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  const session = request.cookies.get('ff_session_id');
+  const { pathname } = request.nextUrl;
+
+  if (!session && pathname !== '/login')
+    return NextResponse.redirect(new URL('/login', request.url));
+
+  if (session && pathname === '/login') {
+    return NextResponse.redirect(new URL('/home', request.url));
+  }
+
+  return NextResponse.next();
+}
+
+// 미들웨어가 작동할 경로 설정
+export const config = {
+  matcher: ['/login', '/home', '/'],
+};
