f924c3e1aac86c2595934c3ece4c979b47476303
BanYeah <04smailing@gmail.com>
2026-01-30 23:25:37 +0900

    Style(be):

--- backend/src/migrations/1768240918571-UserJudgeSchema.ts
+++ backend/src/migrations/1768240918571-UserJudgeSchema.ts
@@ -6,20 +6,20 @@ export class UserJudgeSchema1768240918571 implements MigrationInterface {
       CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
       CREATE TABLE "User" (
-          "user_id"    UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-          "minicode"   VARCHAR(7) NOT NULL,
-          "enter_code" TEXT NOT NULL,
-          "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+        "user_id"    UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+        "minicode"   VARCHAR(7) NOT NULL,
+        "enter_code" TEXT NOT NULL,
+        "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
       );
 
       CREATE UNIQUE INDEX idx_user_minicode_include_enter ON "User"("minicode") INCLUDE ("enter_code");
 
       CREATE TABLE "Judge" (
-          "user_id"    UUID PRIMARY KEY REFERENCES "User"("user_id") ON DELETE CASCADE,
-          "minicode"   VARCHAR(7) NOT NULL,
-          "appointed_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        "user_id"    UUID PRIMARY KEY REFERENCES "User"("user_id") ON DELETE CASCADE,
+        "minicode"   VARCHAR(7) NOT NULL,
+        "appointed_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-          CONSTRAINT "fk_judge_minicode" FOREIGN KEY ("minicode") REFERENCES "User"("minicode")
+        CONSTRAINT "fk_judge_minicode" FOREIGN KEY ("minicode") REFERENCES "User"("minicode")
       );
 
       CREATE UNIQUE INDEX idx_judge_minicode_unique ON "Judge"("minicode");
