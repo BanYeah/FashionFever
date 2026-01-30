dc2d7739e2264ee81c274398015553d4a1a14def
BanYeah <04smailing@gmail.com>
2026-01-30 22:32:13 +0900

    Style(fe):

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,14 +1,14 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const session = request.cookies.get('ff_session_id');
+  const session = request.cookies.get("ff_session_id");
   const { pathname } = request.nextUrl;
 
-  if (!session && pathname !== '/login')
-    return NextResponse.redirect(new URL('/login', request.url));
+  if (!session && pathname !== "/login")
+    return NextResponse.redirect(new URL("/login", request.url));
 
-  if (session && pathname === '/login') {
-    return NextResponse.redirect(new URL('/home', request.url));
+  if (session && pathname === "/login") {
+    return NextResponse.redirect(new URL("/home", request.url));
   }
 
   return NextResponse.next();
@@ -16,5 +16,5 @@ export function middleware(request: NextRequest) {
 
 // 미들웨어가 작동할 경로 설정
 export const config = {
-  matcher: ['/login', '/home', '/'],
+  matcher: ["/login", "/home", "/"],
 };
