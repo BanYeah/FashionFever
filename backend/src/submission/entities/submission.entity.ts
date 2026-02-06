021834ddb8dc7f7234120950418dfe8653b69ff2
BanYeah <04smailing@gmail.com>
2026-02-07 02:17:12 +0900

    Feat(be): Submission Entity

---
+++ backend/src/submission/entities/submission.entity.ts
@@ -0,0 +1,97 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+import { Schedule } from 'src/theme/entities/schedule.entity';
+import { User } from 'src/auth/entities/user.entity';
+
+@Entity('Submission')
+export class Submission {
+  @PrimaryGeneratedColumn({ type: 'bigint' })
+  submission_id: string;
+
+  @Column({ type: 'uuid' })
+  theme_id: string;
+
+  @Column({ type: 'uuid', nullable: true })
+  user_id: string | null;
+
+  @Column({ type: 'text', nullable: true })
+  content_url: string | null;
+
+  @CreateDateColumn({ type: 'timestamptz' })
+  created_at: Date;
+
+  @Column({ type: 'boolean', default: null, nullable: true })
+  is_reviewed: boolean | null;
+
+  @Column({ type: 'timestamptz', default: null, nullable: true })
+  reviewed_at: Date | null;
+
+  @Column({ type: 'integer', default: null, nullable: true })
+  vote_rank: number | null;
+
+  @Column({
+    type: 'numeric',
+    precision: 3,
+    scale: 2,
+    default: 0.0,
+  })
+  vote_score: number;
+
+  @Column({
+    type: 'numeric',
+    precision: 3,
+    scale: 2,
+    default: 0.0,
+  })
+  judge_score: number;
+
+  @Column({
+    type: 'numeric',
+    precision: 3,
+    scale: 2,
+    default: 0.0,
+  })
+  like_score: number;
+
+  @Column({
+    type: 'numeric',
+    precision: 3,
+    scale: 2,
+    default: 0.0,
+  })
+  total_score: number;
+
+  @Column({ type: 'integer', default: null, nullable: true })
+  final_rank: number | null;
+
+  @Column({
+    type: 'numeric',
+    precision: 3,
+    scale: 2,
+    default: 0.0,
+  })
+  adj_score: number;
+
+  @Column({
+    type: 'numeric',
+    precision: 3,
+    scale: 2,
+    default: 0.0,
+  })
+  final_score: number;
+
+  @ManyToOne(() => Schedule, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'theme_id' })
+  theme: Schedule;
+
+  @ManyToOne(() => User, { onDelete: 'SET NULL', nullable: true })
+  @JoinColumn({ name: 'user_id' })
+  user: User | null;
+}
