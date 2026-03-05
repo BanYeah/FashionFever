4140a7938d4a513a66f9d09043dd7d44282aff87
BanYeah <04smailing@gmail.com>
2026-03-05 16:07:44 +0900

    Style(fe):

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,18 +1,15 @@
 import { Flex, Stack } from "@mantine/core";
-import { LoginShell } from "@/components/login/login-shell";
 import { LoginInput } from "@/components/login/login-input";
 import { EventSchedule } from "@/components/login/event-schedule";
 
 export default function LoginPage() {
   return (
-    <LoginShell>
-      <Stack align="stretch" mt={25} ml={15} mr={15} mb={25} gap={0}>
-        <LoginInput />
-        <EventSchedule />
-        <Flex justify="center" mt={25}>
-          <p>테마에 맞게 미니를 개성있게 꾸며주세요 ~ ♥</p>
-        </Flex>
-      </Stack>
-    </LoginShell>
+    <Stack align="stretch" mt={25} ml={15} mr={15} mb={25} gap={0}>
+      <LoginInput />
+      <EventSchedule />
+      <Flex justify="center" mt={25}>
+        <p>테마에 맞게 미니를 개성있게 꾸며주세요 ~ ♥</p>
+      </Flex>
+    </Stack>
   );
 }
