043dac478ba96c8f6e1088c422a1d7eac2880a91
BanYeah <04smailing@gmail.com>
2026-03-07 12:51:14 +0900

    Feat(fe): Issue #21 Set up Script

---
+++ frontend/set-up.sh
@@ -0,0 +1,30 @@
+# First, Set up Backend
+
+## Set .env in Frontend Directory
+
+# Frontend Build and Deploy
+cd frontend
+npm install
+npm run build
+
+pm2 start npm --name "frontend" -- start
+pm2 save
+
+# Nginx
+sudo vim /etc/nginx/sites-available/default
+# server {
+#     server_name $DOMAIN;
+
+#     location / {
+#         proxy_pass http://localhost:3000;
+#         proxy_http_version 1.1;
+#         proxy_set_header Upgrade $http_upgrade;
+#         proxy_set_header Connection 'upgrade';
+#         proxy_set_header Host $host;
+#         proxy_cache_bypass $http_upgrade;
+#     }
+
+#     # SSL Setting Code by Certbot
+# }
+## Modify the location as shown above.
+sudo systemctl restart nginx
